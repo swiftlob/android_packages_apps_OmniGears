ae451d45d8fd95816d1b472afb7b22d3082cb5b9
Vachounet <vachounet@live.fr>
2018-10-16 10:12:01 +0200

    OmniGears: add display colors menu if related package is present
    Change-Id: I85ecf302229fcfa0a0dc592601215b56ae9ebb90

--- src/org/omnirom/omnigears/OmniDashboardFragment.java
+++ src/org/omnirom/omnigears/OmniDashboardFragment.java
@@ -24,6 +24,7 @@ import android.support.v7.preference.Preference;
 import android.provider.SearchIndexableResource;
 
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.omni.PackageUtils;
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -39,12 +40,22 @@ public class OmniDashboardFragment extends DashboardFragment {
     private static final String TAG = "OmniDashboardFragment";
     public static final String CATEGORY_OMNI = "com.android.settings.category.ia.omni";
     private static final String KEY_DEVICE_PARTS = "device_parts";
+    private static final String KEY_DISPLAY_MANAGER = "display_manager";
+
+    private static final String PACKAGE_DEVICE_PARTS = "org.omnirom.device";
+    private static final String PACKAGE_DISPLAY_MANAGER = "org.omnirom.omnidisplaymanager";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         super.onCreatePreferences(savedInstanceState, rootKey);
-        if (!isDevicePartsSupported(getContext())) {
+        if (!PackageUtils.isAvailableApp(PACKAGE_DEVICE_PARTS, getContext())) {
             Preference pref = getPreferenceScreen().findPreference(KEY_DEVICE_PARTS);
+            if (pref != null) {
+                getPreferenceScreen().removePreference(pref);
+            }
+        }
+        if (!PackageUtils.isAvailableApp(PACKAGE_DISPLAY_MANAGER, getContext())) {
+            Preference pref = getPreferenceScreen().findPreference(KEY_DISPLAY_MANAGER);
             if (pref != null) {
                 getPreferenceScreen().removePreference(pref);
             }
@@ -66,16 +77,6 @@ public class OmniDashboardFragment extends DashboardFragment {
         return R.xml.omni_dashboard_fragment;
     }
 
-    private static boolean isDevicePartsSupported(Context context) {
-        boolean devicePartsSupported = false;
-        try {
-            devicePartsSupported = context.getPackageManager().getPackageInfo(
-                    "org.omnirom.device", 0).versionCode > 0;
-        } catch (PackageManager.NameNotFoundException e) {
-        }
-        return devicePartsSupported;
-    }
-
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider() {
 
@@ -89,7 +90,7 @@ public class OmniDashboardFragment extends DashboardFragment {
                 @Override
                 public List<String> getNonIndexableKeys(Context context) {
                     ArrayList<String> result = new ArrayList<String>();
-                    if (!isDevicePartsSupported(context)) {
+                    if (!PackageUtils.isAvailableApp(PACKAGE_DEVICE_PARTS, context)) {
                         result.add(KEY_DEVICE_PARTS);
                     }
                     return result;
